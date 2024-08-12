2010fb38ccc85b5e529d68e427e4926fbf80cd94
robotncom <136340731+HoangNt1398@users.noreply.github.com>
2024-08-12 09:48:31 +0900

    start project

---
+++ src/main/java/com/nthoang/ktech_springboot_project/KtechSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nthoang.ktech_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtechSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtechSpringbootProjectApplication.class, args);
+	}
+
+}
